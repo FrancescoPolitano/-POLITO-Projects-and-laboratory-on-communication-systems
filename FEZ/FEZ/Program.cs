90896060981cba472fbd4329fbb2453012a98704
Cristiano <cristianopalazzi@gmail.com>
2017-12-06 20:55:19 +0100

    FEZ - take photo and send to server ServerFEZ -receive photo from FEZ -added ZXing library

---
+++ FEZ/FEZ/Program.cs
@@ -0,0 +1,94 @@
+﻿using Gadgeteer.Modules.GHIElectronics;
+using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using System.Net.Sockets;
+using System.Net;
+
+namespace FEZ
+{
+    public partial class Program
+    {
+        void ProgramStarted()
+        {
+            camera.CameraDisconnected += Camera_CameraDisconnected;
+            camera.CameraConnected += Camera_CameraConnected;
+            ethernetJ11D.UseThisNetworkInterface();
+            ethernetJ11D.NetworkUp += EthernetJ11D_NetworkUp;
+            ethernetJ11D.NetworkDown += EthernetJ11D_NetworkDown;
+            button.ButtonPressed += Button_ButtonPressed;
+            camera.PictureCaptured += Camera_PictureCaptured;
+            new Thread(RunWebServer).Start();
+        }
+
+        private void Camera_PictureCaptured(Camera sender, GT.Picture e)
+        {
+            Debug.Print("Picture taken");
+            byte[] image = e.PictureData;
+            int pictureSize = image.Length;
+            Debug.Print("Send picture");
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("192.168.100.1"), 8989);
+            Socket sockSender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sockSender.Connect(remoteEP);
+            int sent = sockSender.Send(BitConverter.GetBytes(pictureSize), 0, sizeof(int), SocketFlags.None);
+            sent = 0;
+            //TODO handle socket exception
+            while (sent < pictureSize)
+            {
+                if (pictureSize - sent > 1400)
+                    sent += sockSender.Send(image, sent, 1400, SocketFlags.None);
+                else sent += sockSender.Send(image, sent, pictureSize - sent, SocketFlags.None);
+                if (sent == pictureSize)
+                    Debug.Print("FINISHED");
+            }
+            sockSender.Close();
+            Debug.Print("sent " + sent);
+        }
+
+        private void Button_ButtonPressed(Button sender, Button.ButtonState state)
+        {
+            camera.TakePicture();
+        }
+
+        private void Camera_CameraConnected(Camera sender, EventArgs e)
+        {
+            camera.CurrentPictureResolution = Camera.PictureResolution.Resolution176x144;
+            Debug.Print("Camera connessa");
+        }
+
+        private void Camera_CameraDisconnected(Camera sender, EventArgs e)
+        {
+            Debug.Print("Camera disconnessa");
+        }
+
+        private void RunWebServer()
+        {
+            while (ethernetJ11D.IsNetworkUp == false)
+            {
+                Debug.Print("Waiting...");
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void EthernetJ11D_NetworkDown(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        {
+            Debug.Print("Network is down!");
+        }
+
+        private void EthernetJ11D_NetworkUp(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        {
+            Debug.Print("Network is up!");
+
+        }
+    }
+}
---
+++ ServerFEZ/ServerFEZ/Server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+
+namespace ServerFEZ
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            //new Server();
+            var barcodeReader = new BarcodeReader();
+            var barcodeBitmap = (Bitmap)Bitmap.FromFile(@"C:\Users\Cristiano\Desktop\jpeg.jpg");
+            var barcodeResult = barcodeReader.Decode(barcodeBitmap);
+
+            Console.WriteLine($"Decoded barcode text: {barcodeResult?.Text}");
+            Console.WriteLine($"Barcode format: {barcodeResult?.BarcodeFormat}");
+        }
+
+        public Server()
+        {
+            localEndPoint = new IPEndPoint(IPAddress.Parse("192.168.100.1"), 8989);
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            startServer();
+        }
+
+        private void startServer()
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(1);
+            while (true)
+            {
+                Console.WriteLine("Waiting for picture...");
+                Socket handler = listener.Accept();
+                Console.WriteLine("FEZ CONNECTED");
+                receiveFromFez(handler);
+            }
+        }
+
+        private void receiveFromFez(Socket handler)
+        {
+            //TODO change
+            handler.ReceiveTimeout = 0;
+            handler.SendTimeout = 0;
+            int received = 0;
+            byte[] pictureByteSize = new byte[sizeof(int)];
+            received = handler.Receive(pictureByteSize, 0, sizeof(int), SocketFlags.None);
+            int pictureSize = BitConverter.ToInt32(pictureByteSize, 0);
+            Console.WriteLine("PICTURE SIZE {0} ", pictureSize);
+            byte[] pictureData = new byte[pictureSize];
+            received = 0;
+            SocketError error;
+            while (received < pictureSize)
+            {
+                if (pictureSize - received > 1400)
+                    received += handler.Receive(pictureData, received, 1400, SocketFlags.None, out error);
+                else received += handler.Receive(pictureData, received, pictureSize - received, SocketFlags.None, out error);
+            }
+            Console.WriteLine("PictureSize: {0},received {1} ", pictureSize, received);
+            handler.Close();
+            Image x = (Bitmap)((new ImageConverter()).ConvertFrom(pictureData));
+            x.Save(@"C:\Users\Cristiano\Desktop\image.jpeg", ImageFormat.Jpeg);
+        }
+
+        private IPEndPoint localEndPoint;
+        private Socket listener;
+    }
+}

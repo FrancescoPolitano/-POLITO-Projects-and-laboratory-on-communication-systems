3533e30371b3c1f05a519680435ec0a7bfbeb513
Cristiano <cristianopalazzi@gmail.com>
2018-01-05 19:50:21 +0100

    fix complexQuery

--- RestfulService/src/rest/ComplexQuery.java
+++ RestfulService/src/rest/ComplexQuery.java
@@ -1,5 +1,6 @@
 package rest;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -48,40 +49,59 @@ public class ComplexQuery {
 		End = end;
 	}
 
+	@SuppressWarnings("deprecation")
 	public String toValidSQLQuery() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT l.Name, a.TimeS, e.SerialNumber, e.Name, e.Surname, a.Result "
-				+ "FROM locals l ,employes e ,accesses a  " + "WHERE ");
+				+ "FROM locals l ,employes e ,accesses a WHERE a.IdEmployee = e.SerialNumber AND a.IdLocal = l.Id");
+
+		if (Employees.size() != 0)
+			sb.append(" AND ");
 
 		StringBuilder employees = new StringBuilder("");
+
 		for (Iterator<String> it = Employees.iterator(); it.hasNext();) {
 			String element = it.next();
 			employees.append("e.SerialNumber='" + element + "'");
-			if (it.hasNext()) {
-				sb.append(" OR ");
-			}
+			if (it.hasNext())
+				employees.append(" OR ");
 		}
-		if (!employees.toString().equals("")) {
-			sb.append(" ( ").append(employees.toString()).append(" ) AND ");
-		}
+
+		if (!employees.toString().equals(""))
+			sb.append(" ( ").append(employees.toString()).append(" )");
+
+		if (Rooms.size() != 0)
+			sb.append(" AND ");
 
 		StringBuilder rooms = new StringBuilder("");
 		for (Iterator<String> it = Rooms.iterator(); it.hasNext();) {
 			String element = it.next();
 			rooms.append("l.Id='" + element + "'");
-			if (it.hasNext()) {
-				sb.append(" OR ");
-			}
+			if (it.hasNext())
+				rooms.append(" OR ");
 		}
 
 		if (!rooms.toString().equals(""))
-			sb.append(" ( ").append(rooms.toString()).append(" ) AND ");
+			sb.append(" ( ").append(rooms.toString()).append(" ) ");
 
-		if (getEnd() != null)
-			sb.append("( a.TimeS >'" + getInitial() + "' AND a.TimeS <'" + getEnd() + "')");
-		else
-			sb.append("( a.TimeS='" + getInitial() + "')");
+		if (getEnd() != null && getInitial() != null) {
+			String start, end;
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			getInitial().setHours(23);
+			getInitial().setMinutes(59);
+			getInitial().setSeconds(59);
+			getEnd().setHours(23);
+			getEnd().setMinutes(59);
+			getEnd().setSeconds(59);
+			start = sdf.format(getInitial());
+			end = sdf.format(getEnd());
 
+			sb.append(" AND ");
+			if (getEnd() != null)
+				sb.append("( a.TimeS >'" + start + "' AND a.TimeS <'" + end + "')");
+			else
+				sb.append("( a.TimeS='" + start + "')");
+		}
 		return sb.toString();
 	}
 }

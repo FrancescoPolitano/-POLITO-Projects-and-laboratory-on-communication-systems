66563456a0f84b5bf9f3427e542933e056c4052c
Gianmaria Tremigliozzi <gianmaria.tremigliozzi@gmail.com>
2018-01-09 19:41:56 +0100

    Serious series: Push

--- GUI/GUI/RestClient.cs
+++ GUI/GUI/RestClient.cs
@@ -28,20 +28,28 @@ namespace GUI
         //Login, saves a "cookie"
         public static bool Login(LoginData logindata)
         {
-            client.Timeout = new TimeSpan(0, 0, 60);
+            client.Timeout = TimeSpan.FromSeconds(30);
             string json = JsonConvert.SerializeObject(logindata);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/login", content).Result;
-            if (response.IsSuccessStatusCode)
+            try
             {
-                CookieCollection responseCookies = cookies.GetCookies(new Uri(myRest + "/login"));
-                if (responseCookies["Token"] != null)
-                    sessionToken = responseCookies["Token"].Value;
+                HttpResponseMessage response = client.PostAsync(myRest + "/login", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    CookieCollection responseCookies = cookies.GetCookies(new Uri(myRest + "/login"));
+                    if (responseCookies["Token"] != null)
+                        sessionToken = responseCookies["Token"].Value;
 
-                if (String.IsNullOrEmpty(sessionToken))
-                    return false;
-                else
-                    return true;
+                    if (String.IsNullOrEmpty(sessionToken))
+                        return false;
+                    else
+                        return true;
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
             }
             return false;
         }
@@ -49,11 +57,19 @@ namespace GUI
         //logout
         public static void Logout()
         {
-            HttpResponseMessage response = client.GetAsync(myRest + "/logout").Result;
-            if (!response.IsSuccessStatusCode)
-                throw new Exception();
-            else
-                sessionToken = String.Empty;
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(myRest + "/logout").Result;
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception();
+                else
+                    sessionToken = String.Empty;
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
         }
 
 
@@ -61,12 +77,20 @@ namespace GUI
         public static List<Employee> GetAllUsers()
         {
             string json = String.Empty;
-            HttpResponseMessage response = client.GetAsync(myRest + "/users/employees").Result;
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                List<Employee> lista = JsonConvert.DeserializeObject<List<Employee>>(response.Content.ReadAsStringAsync().Result);
-                return lista;
+                HttpResponseMessage response = client.GetAsync(myRest + "/users/employees").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Employee> lista = JsonConvert.DeserializeObject<List<Employee>>(response.Content.ReadAsStringAsync().Result);
+                    return lista;
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
             }
             return null;
         }
@@ -75,12 +99,22 @@ namespace GUI
         public static List<Visitor> GetAllVisitors()
         {
             string json = String.Empty;
-            HttpResponseMessage response = client.GetAsync(myRest + "/users/visitors").Result;
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                List<Visitor> lista = JsonConvert.DeserializeObject<List<Visitor>>(response.Content.ReadAsStringAsync().Result);
-                return lista;
+
+
+                HttpResponseMessage response = client.GetAsync(myRest + "/users/visitors").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Visitor> lista = JsonConvert.DeserializeObject<List<Visitor>>(response.Content.ReadAsStringAsync().Result);
+                    return lista;
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
             }
             return null;
         }
@@ -89,10 +123,18 @@ namespace GUI
         public static List<Room> GetAllRooms()
         {
             string json = String.Empty;
-            HttpResponseMessage response = client.GetAsync(myRest + "/locals").Result;
-            if (response.IsSuccessStatusCode)
+            try
             {
-                return JsonConvert.DeserializeObject<List<Room>>(response.Content.ReadAsStringAsync().Result);
+                HttpResponseMessage response = client.GetAsync(myRest + "/locals").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<List<Room>>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
             }
             return null;
         }
@@ -108,9 +150,17 @@ namespace GUI
             };
             string json = JsonConvert.SerializeObject(eRC);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/users/employees", content).Result;
-            if (response.IsSuccessStatusCode)
-                return JsonConvert.DeserializeObject<EmployeeResponseClass>(response.Content.ReadAsStringAsync().Result);
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/users/employees", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return JsonConvert.DeserializeObject<EmployeeResponseClass>(response.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
             return null;
         }
 
@@ -119,11 +169,19 @@ namespace GUI
         {
             string json = JsonConvert.SerializeObject(v);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PostAsync(myRest + "/users/visitors", content);
-            if (response.IsSuccessStatusCode)
-                return response.Content.ReadAsStringAsync().Result;
-            else
-                return String.Empty;
+            try
+            {
+
+                HttpResponseMessage response = await client.PostAsync(myRest + "/users/visitors", content);
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
+            return String.Empty;
         }
 
         //method to get a single user (never used)
@@ -137,16 +195,23 @@ namespace GUI
         }
 
         //method to block accesses from user 
-        public static  bool BlockAccess(int serial)
+        public static bool BlockAccess(int serial)
         {
             //TODO test
             string json = JsonConvert.SerializeObject(serial);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response =  client.PostAsync(myRest + "/users/block", content).Result;
-            if (response.IsSuccessStatusCode)
-                return true;
-            else
-                return false;
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/users/block", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return true;
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
+            return false;
         }
 
         //TODO CHANGE THIS WITH MODIFY USERS
@@ -155,11 +220,18 @@ namespace GUI
         {
             string json = JsonConvert.SerializeObject(auth);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/users/authLevel", content).Result;
-            if (response.IsSuccessStatusCode)
-                return true;
-            else
-                return false;
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/users/authLevel", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return true;
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
+            return false;
         }
 
         //method to ask for a change in QRcode
@@ -167,23 +239,38 @@ namespace GUI
         {
             string json = JsonConvert.SerializeObject(id);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/users/new_code", content).Result;
-            if (response.IsSuccessStatusCode)
-                return response.Content.ReadAsStringAsync().Result;
-            else
-                return String.Empty;
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/users/new_code", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
+            return String.Empty;
         }
 
 
-       //method to get the accesses. 
+        //method to get the accesses. 
         public static List<Access> GetHistory(ComplexQuery q)
         {
             string json = JsonConvert.SerializeObject(q);
             Console.WriteLine(json);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/query", content).Result;
-            if (response.IsSuccessStatusCode)
-                return JsonConvert.DeserializeObject<List<Access>>(response.Content.ReadAsStringAsync().Result);
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/query", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return JsonConvert.DeserializeObject<List<Access>>(response.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception e)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
             return null;
         }
 
@@ -191,11 +278,18 @@ namespace GUI
         {
             string json = JsonConvert.SerializeObject(e);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = client.PostAsync(myRest + "/users/modify", content).Result;
-            if (response.IsSuccessStatusCode)
-                return true;
-            else
-                return false;
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(myRest + "/users/modify", content).Result;
+                if (response.IsSuccessStatusCode)
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                //TODO find a way to debug it properly
+                Console.WriteLine("TIMEOUT???");
+            }
+            return false;
 
         }
 
